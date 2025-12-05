23488f20c78d3328524bb853225b19f04e82dbf0
alexandrarh <alexavndrarh@gmail.com>
2025-12-05 08:19:37 -0800

    Update _request and _get_access_token

--- src/timesolv_api/timesolv_api.py
+++ src/timesolv_api/timesolv_api.py
@@ -8,6 +8,7 @@ import os
 import sys
 from datetime import datetime, timedelta
 from typing import List, Dict, Set, Optional
+from urllib.parse import urljoin
 
 class TimeSolvAPIError(Exception):
     '''Custom exception for TimeSolv API errors.'''
@@ -27,11 +28,11 @@ class TimeSolvAuth:
 class TimeSolvAPI:
     '''Client for TimeSolv API.'''
 
-    def __init__(self, config_data: Dict):
-        access_token = self._get_access_token(config_data)
-        
+    def __init__(self, config_data: Dict, base_url: str='https://apps.timesolv.com/Services/rest/oAuth2V1/'):
+        self.base_url = base_url
+        self.access_token = self._get_access_token(config_data)
         self.headers = {
-            "Authorization": f"Bearer {access_token}",
+            "Authorization": f"Bearer {self.access_token}",
             "Content-Type": "application/json"
         }
 
@@ -51,20 +52,31 @@ class TimeSolvAPI:
             }
 
             # TODO: Call _request helper instead
-            response = requests.post('https://apps.timesolv.com/Services/rest/oAuth2V1/Token', data=access_data)
-            token_data = response.json()
-
-            if token_data.get("error"):
-                raise TimeSolvAPIError(f"Error obtaining access token: {token_data['error_description']}")
-
+            token_data = self._request('POST', 'Token', payload=access_data)
             return token_data['access_token']
+            
+            # token_data = response.json()
+
+            # if token_data.get("error"):
+            #     raise TimeSolvAPIError(f"Error obtaining access token: {token_data['error_description']}")
 
         raise TimeSolvAPIError("Missing required authentication parameters.")
     
     # Helper
-    def _request(self, method: str):
+    def _request(self, method: str, endpoint: str, payload: Optional[Dict]=None, **kwargs) -> List[Dict]:
         '''Make a request to the TimeSolv API.'''
-        pass
+        full_url = urljoin(self.base_url, endpoint)
+
+        try:
+            response = requests.request(method, full_url, headers=self.headers, json=payload, **kwargs)       # Add timeout parameter if needed; also unsure of kwargs
+            response.raise_for_status()
+            return response.json()
+        except requests.HTTPError as e:
+            raise TimeSolvAPIError(f"HTTP error occurred: {e} - Response: {response.text}")
+        except requests.RequestException as e:
+            raise TimeSolvAPIError(f"Error making request to {full_url}: {e}")
+        except ValueError as e:
+            raise TimeSolvAPIError(f"Error parsing JSON response from {full_url}: {e}")
     
     # POST
     def get_firm_users(self) -> List[Dict]:
